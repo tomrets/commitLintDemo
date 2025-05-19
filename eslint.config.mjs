7dc8e2d759b22a1ac53038b79b9eef702eb2ce62
tomrets <64450913+tomrets@users.noreply.github.com>
2025-05-19 09:19:17 +0000

    feat(self): eslint config done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,25 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
+  // pluginReact.configs.flat.recommended
+  {
+    plugins: {
+      react: pluginReact
+    }
+  },
+  {
+    rules: {
+      "react/jsx-uses-vars": "error",
+      "react/jsx-uses-react": "error",
+      "react/react-in-jsx-scope": "off" // 关闭 React 作用域检查
+      // "react/jsx-runtime": "error" // 启用 JSX 运行时自动导入
+    }
+  }
 ]);
